4fe4297a4a78ee8473292f48a898b2b3bbe34e5d
hunki-lee <hkuni.lee@gmail.com>
2026-08-28 08:15:59 +0900

    Update SALES_NOTICE message in script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   "use strict";
 
   var BANK_ACCOUNT = "농협 623083-56-013585 박민자";
-  var SALES_NOTICE = "천하제일도(딱복)\n- 4kg 9-11과: 4.7만\n- 4kg 12과: 4.2만\n- 4kg 13-14과: 3.7만\n- 4kg 17-18과: 2.7만";
+  var SALES_NOTICE = "2026년 판매마감 \n 감사합니다^^";
 
   var fields = {
     senderName: document.getElementById("senderName"),
